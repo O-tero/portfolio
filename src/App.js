27771aecb9bab4270d7fb55a553516f726216af6
Otero <oteronickbrian@gmail.com>
2024-11-12 16:26:45 +0300

    merge conflicting css modules

--- src/App.js
+++ src/App.js
@@ -9,22 +9,24 @@ import Offerings from './components/Offerings';
 import RateCard from './components/RateCard';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
-import './styles/App.module.css';
+import styles from './styles/App.module.css';
 
 const App = () => {
   return (
     <Router>
-      <div>
+      <div className={styles.mainContainer}>
         <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/music-mission" element={<MusicMission />} />
-          <Route path="/achievements" element={<Achievements />} />
-          <Route path="/initiatives" element={<Initiatives />} />
-          <Route path="/offerings" element={<Offerings />} />
-          <Route path="/rate-card" element={<RateCard />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
+        <div className={styles.contentWrapper}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/music-mission" element={<MusicMission />} />
+            <Route path="/achievements" element={<Achievements />} />
+            <Route path="/initiatives" element={<Initiatives />} />
+            <Route path="/offerings" element={<Offerings />} />
+            <Route path="/rate-card" element={<RateCard />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </div>
         <Footer />
       </div>
     </Router>
