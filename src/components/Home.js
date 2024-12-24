c222204bc0fd770b51f2115c150863ad21eed103
brianmaseno <brianmmayoga@gmail.com>
2024-12-24 12:37:35 +0300

    Add 'Poppins' font to various styles and update Home component layout

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,47 +1,22 @@
 import React from 'react';
-import Slider from 'react-slick';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
 import styles from '../styles/Home.module.css';
-import profileImage from '../assets/profile.jpeg';
-import cop27_1Image from '../assets/cop27_1.jpg';
-import cop28 from '../assets/cop28.jpg';
-import image5 from '../assets/image5.jpg';
+import backgroundImage from '../assets/image9.jpg';
+
 
 const Home = () => {
-  const images = [profileImage, cop27_1Image, cop28, image5]; 
-
-  const sliderSettings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 3000,
-    pauseOnHover: true,
-  };
-
   return (
-    <div className={styles.container}>
-      <h1 className={styles.heading}>About Me</h1>
-      <div className={styles.sliderContainer}>
-        <Slider {...sliderSettings}>
-          {images.map((image, index) => (
-            <div key={index}>
-              <img src={image} alt={`Slide ${index}`} className={styles.sliderImage} />
-            </div>
-          ))}
-        </Slider>
+    <div className={styles.container} style={{ backgroundImage: `url(${backgroundImage})` }}>
+      <div className={styles.textContainer}>
+        <h1 className={styles.heading}>About Me</h1>
+        <p className={styles.description}>
+          I’m Jesse David, a climate justice advocate, sustainable musician, and African United Nations Youth Fellow for 2024.
+          I blend my passion for environmental justice with the power of music to inspire change.
+          {/* My music is a tool for impact, bridging the gap between art and activism, and serving
+          as an anthem for the voices of frontline communities affected by the climate crisis.
+          Through my journey, I’ve had the privilege of representing African youth on global 
+          platforms such as COP27 and COP28, using the power of music to call for action. */}
+        </p>
       </div>
-      <p className={styles.description}>
-        I’m Jesse David, a climate justice advocate, sustainable musician, and African United Nations Youth Fellow for 2024.
-        I blend my passion for environmental justice with the power of music to inspire change.
-        My music is a tool for impact, bridging the gap between art and activism, and serving
-        as an anthem for the voices of frontline communities affected by the climate crisis.
-        Through my journey, I’ve had the privilege of representing African youth on global 
-        platforms such as COP27 and COP28, using the power of music to call for action.
-      </p>
     </div>
   );
 };
